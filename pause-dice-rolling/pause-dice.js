9a11aef35d2fdec763a33373a99c22c65fac267f
MrTranscended <74849518+MrTranscended@users.noreply.github.com>
2025-05-17 19:54:35 -0700

    Delete pause-dice-rolling directory

--- pause-dice-rolling/pause-dice.js
+++
@@ -1,8 +0,0 @@
-Hooks.on("preCreateChatMessage", (message, options, userId) => {
-  // Check if the game is paused and if the message contains dice rolls
-  if (game.paused && message.rolls && message.rolls.length > 0) {
-    ui.notifications.warn("The game is currently paused. You cannot roll dice.");
-    return false; // Prevent the message from being created
-  }
-  return true;
-});
